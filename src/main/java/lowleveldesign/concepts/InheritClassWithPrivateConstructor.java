5054be7fa3e0e26f790f11fbc8bb523adbe391d6
Aman Garg <aman@L-TDQWT9DP7Q.local>
2024-03-30 13:40:04 +0530

    Inherit class with private constructor

---
+++ src/main/java/lowleveldesign/concepts/InheritClassWithPrivateConstructor.java
@@ -0,0 +1,11 @@
+package lowleveldesign.concepts;
+
+public class InheritClassWithPrivateConstructor {
+    private InheritClassWithPrivateConstructor() {
+
+    }
+}
+
+//class SubClass extends InheritClassWithPrivateConstructor {// doesn't compile
+//
+//}
